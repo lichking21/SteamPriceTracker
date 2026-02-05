101d4645921a3a0fbd1908d4babecf0ce671915f
lichking21 <kazakovaman@gmail.com>
2026-02-05 14:15:20 +0100

    (ADD) Response structure for parsing GameDetails json

---
+++ Network/ResponceStructure.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+
+namespace Network;
+
+public class PriceOverview
+{
+    [JsonPropertyName("discount_percent")]
+    public int DiscountPercent {get; set;}
+
+    [JsonPropertyName("initial_formatted")]
+    public string InitialPrice {get;set;} = "";
+
+    [JsonPropertyName("final_formatted")]
+    public string FinalPrice {get;set;} = "";
+}
+
+public class GameData
+{
+    [JsonPropertyName("data")]
+    public PriceOverview? Price {get;set;}
+
+    [JsonPropertyName("is_free")]
+    public bool IsFree {get;set;}
+}
+
+public class GameDetails
+{
+    [JsonPropertyName("success")]
+    public bool Success {get;set;}
+
+    [JsonPropertyName("data")]
+    public GameData? Data {get;set;}
+}
