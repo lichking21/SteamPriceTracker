2da9fe7b564c83f6986de474785d51007a8c2bb8
lichking21 <kazakovaman@gmail.com>
2026-02-12 09:27:04 +0100

    (ADD) Users table in database

---
+++ DataBaseOperator/Entity/UserItem.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataBaseOperator.Entities;
+
+[Table("users")]
+public class UserItem
+{
+    [Key]
+    [Column("ID")]
+    public int ID {get;set;}
+
+    [Column("Name")]
+    public string Name {get;set;} = "UNKNOWN";
+
+    public UserItem() {}
+
+    public UserItem(string name)
+    {
+        Name = name;
+    }
+}
---
+++ DataBaseOperator/UserDB.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using DataBaseOperator.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DataBaseOperator;
+
+public class UserDB
+{
+    private readonly ApplicationContext _context;
+    private readonly ILogger<UserDB> _logger;
+
+    public UserDB(ApplicationContext context, ILogger<UserDB> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Use it to get information about user id by his name
+    /// </summary>
+    /// <returns>Id as integer value type</returns>
+    public async Task<int> GetUserId(string name)
+    {
+        int id = await _context.users
+            .Where(u => u.Name != null && EF.Functions.ILike(u.Name, $"%{name}%"))
+            .Select(user => user.ID)
+            .FirstOrDefaultAsync();
+
+        _logger.LogInformation($"(LOG) >> User {name}'s id: {id}");
+
+        return id;
+    }
+
+    public async Task AddUserItem(UserItem item)
+    {
+        bool ifExists = await _context.users.AnyAsync(u => u.ID ==item.ID);
+
+        if (ifExists == false)
+        {
+            item.Name = string.IsNullOrEmpty(item.Name) ? "UNKNOWN" : item.Name;
+
+            _context.users.Add(item);
+
+            await _context.SaveChangesAsync();
+            _logger.LogInformation($"(LOG) >> User {item.Name} was added to users table");
+        }
+        else
+        {
+            _logger.LogWarning($"(WARN) >> User {item.Name} already exists in users table");
+        }
+    }
+}
