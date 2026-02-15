90f4f6077d918edc835611c4a609939c77eecfcc
lichking21 <kazakovaman@gmail.com>
2026-02-15 11:51:00 +0100

    (ADD) New method AddById

--- DataBaseOperator/UserWishlistService.cs
+++ DataBaseOperator/UserWishlistService.cs
@@ -5,16 +5,16 @@ using Network;
 
 namespace DataBaseOperator;
 
-public class DBService
+public class UserWishlistService
 {
     private MainDB _mainDB;
     private TrackedGamesDB _trackedGamesDB;
     private UserWishlistDB _userWishlistDB;
     private Price _price;
-    private readonly ILogger<DBService> _logger;
+    private readonly ILogger<UserWishlistService> _logger;
 
-    public DBService(MainDB mainDB, TrackedGamesDB trackedGamesDB, UserWishlistDB userWishlistDB, 
-                    ILogger<DBService> logger, Price price)
+    public UserWishlistService(MainDB mainDB, TrackedGamesDB trackedGamesDB, UserWishlistDB userWishlistDB, 
+                    ILogger<UserWishlistService> logger, Price price)
     {
         _mainDB = mainDB;
         _trackedGamesDB = trackedGamesDB;
@@ -23,30 +23,47 @@ public class DBService
         _price = price;
     } 
 
-    public async Task AddToUserWishlist(long userId, string gameTitle)
+    /// <summary>
+    /// Use this method to add game to user's wishlist by it's title 
+    /// </summary>
+    public async Task AddByID(long userId, int gameId)
     {
-        int gameId = await _mainDB.GetGameID(gameTitle);
-        if (gameId == 0)
+        string gameTitle = await _mainDB.GetGameTitle(gameId);
+        if (string.IsNullOrEmpty(gameTitle))
         {
-            _logger.LogError($"(ERR) >> Game: {gameTitle} wasn't found");
+            _logger.LogError($"(ERR) >> Game with ID [{gameId}] not found");
             return;
         }
 
         bool isTracked = await _trackedGamesDB.IsTracking(gameId);
+
         if (isTracked == false)
         {
             (string finalPrice, int discount) = await _price.GetPrice(gameId);
-
             var newTrackItem = new TrackedGamesItem(gameId, finalPrice, discount, gameTitle);
-            
             await _trackedGamesDB.AddTrackingGame(newTrackItem);
         }
 
         bool added = await _userWishlistDB.AddLink(userId, gameId);
 
         if (added)
-            _logger.LogInformation($"(LOG) >> {gameTitle} was added to your wishlist");
+            _logger.LogInformation($"(LOG) >> Game [{gameTitle}] was added to your wishlist");
         else
-            _logger.LogWarning($"(WARN) >> {gameTitle} is already in your wishlist");
+            _logger.LogWarning($"(WARN) >> Game [{gameTitle}] is already in your wishlist");
+    }
+
+    /// <summary>
+    /// Use this method to add game to user's wishlist by it's title
+    /// </summary>
+    public async Task AddByTitle(long userId, string gameTitle)
+    {
+        int gameId = await _mainDB.GetGameID(gameTitle);
+        if (gameId == 0)
+        {
+            _logger.LogError($"(ERR) >> Game [{gameTitle}] not found");
+            return;
+        }
+
+        await AddByID(userId, gameId);
     }
 }
