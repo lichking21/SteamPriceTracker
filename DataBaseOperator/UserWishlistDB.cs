31518fa330b5df9aadebc5ad8dc95502ec388846
lichking21 <kazakovaman@gmail.com>
2026-02-15 11:04:26 +0100

    (ADD) New methods to remove game by title or id

--- DataBaseOperator/UserWishlistDB.cs
+++ DataBaseOperator/UserWishlistDB.cs
@@ -1,6 +1,7 @@
 using DataBaseOperator.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 
 namespace DataBaseOperator;
 
@@ -8,7 +9,6 @@ public class UserWishlistDB
 {
     private readonly ILogger<UserWishlistDB> _logger;
     private readonly ApplicationContext _context;
-
     public UserWishlistDB(ILogger<UserWishlistDB> logger, ApplicationContext context)
     {
         _logger = logger;
@@ -16,7 +16,7 @@ public class UserWishlistDB
     }
 
     /// <summary>
-    /// Lnks tracking game's IDs with user's ID  
+    /// Links tracking games IDs with user's ID  
     /// </summary>
     public async Task<bool> AddLink(long userId, int gameId)
     {
@@ -36,9 +36,9 @@ public class UserWishlistDB
     }
 
     /// <summary>
-    /// Use this function to get all games from user's wishlist 
+    /// Use this method to get all games from user's wishlist 
     /// </summary>
-    /// <returns>List of game title</returns>
+    /// <returns>List of game titles</returns>
     public async Task<List<string?>> GetGamesFromWishlist(long userId)
     {
         bool exists = await _context.usersWishlist.AnyAsync(uw => uw.UserId == userId);
@@ -48,7 +48,6 @@ public class UserWishlistDB
             return new List<string?>{};
         }
         
-
         var titles = await _context.usersWishlist
             .Join(_context.trackedGames, // Link usersWishlist table with trackedGames table 
                 uw => uw.GameId, // Takes the key from the 1st table
@@ -57,5 +56,41 @@ public class UserWishlistDB
             ).ToListAsync();
         
         return titles;
+    }
+
+    /// <summary>
+    /// Use this method to remove game from user's wishlist by it's ID
+    /// </summary>
+    public async Task RemoveGameById(long userId, int gameId)
+    {
+        int deletedRows = await _context.usersWishlist
+            .Where(uw => uw.UserId == userId && uw.GameId == gameId)
+            .ExecuteDeleteAsync();
+
+        if (deletedRows == 0)
+        {
+            _logger.LogError($"(ERR) >> Pair [User: {userId}; Game: {gameId}] doesn't exists");
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Use this method to remove game from user's wishlist by it's Title
+    /// </summary>
+    /// <returns></returns>
+    public async Task RemoveGameByTitle(long userId, string title)
+    {
+        int gameId = await _context.trackedGames
+            .Where(tg => tg.Title == title)     
+            .Select(tg => tg.GameId)
+            .FirstOrDefaultAsync();  
+
+        if (gameId == 0)
+        {
+            _logger.LogError($"Game [{title}] not found");
+            return;
+        }
+
+        await RemoveGameById(userId, gameId);
     }
 }
